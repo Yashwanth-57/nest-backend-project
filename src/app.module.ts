9fef9a0eb0616b2e2c975c0ef86d4a0c7b586785
Yashwanth <yashwanth567@gmail.com>
2025-12-08 23:41:35 +0530

    chore: initialize Nest.js project and install dependencies

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres', //  your DB username
+      password: '123456', //  replace with your PostgreSQL password
+      database: 'mydb', //  use the main DB or create new
+      autoLoadEntities: true, //  load all entities automatically
+      synchronize: true, //  auto-creates tables (only for dev)
+    }),
+  ],
+})
+export class AppModule {}
